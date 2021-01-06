12f1dc3a2cce719a3428587d7c52618e9f5cd885
Aditya Mhatre <aditya.r.mhatre@gmail.com>
2021-01-05 19:11:56 -0600

    port

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.get("/*", (req, res) => {
     res.send('Not Allowed')
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server listening on port ${process.env.PORT || 3000}!`);
+app.listen(process.env.PORT || 80, () => {
+    console.log(`Server listening on port ${process.env.PORT || 80}!`);
 });
