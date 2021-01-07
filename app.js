5661a025130096e55ecbf03927e20288aa1edb2c
Aditya Mhatre <aditya.r.mhatre@gmail.com>
2021-01-07 15:45:03 -0600

    refactor + update booking notification

--- app.js
+++ app.js
@@ -1,55 +1,46 @@
 import express from 'express';
 import admin from 'firebase-admin';
 import bodyParser from 'body-parser'
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express()
 const jsonParser = bodyParser.json()
 
-
 const serviceAccount = JSON.parse(process.env.service_account_json)
-const databaseUrl = process.env.firebase_rtdb_url
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: databaseUrl
 });
 
 
-const defaultDatabase = admin.database()
-
-const addNewBooking = (body) => {
-    defaultDatabase.ref('/notifications/newBookings/').push({ ...body })
-}
-
-defaultDatabase.ref('notifications/newBookings/').on('child_added', snapshot => {
-    var topic = 'new-booking-topic';
-
+const sendNotificationToTopic = (topic, data) => {
     var message = {
-        data: snapshot.val(),
-        topic: topic
+        topic, data: { ...data, topic, notificationId: uuidv4() }
     };
 
     admin.messaging().send(message)
         .then((response) => {
-            snapshot.ref.remove()
             console.log('Successfully sent message:', response);
         })
         .catch((error) => {
             console.log('Error sending message:', error);
         });
+}
 
-}, errorObject => {
-    console.log("The read failed: " + errorObject.code)
-})
 
-app.post('/notifications/newBookings', jsonParser, (req, res) => {
-    addNewBooking(req.body)
+app.post('/notifications/newBookingCreated', jsonParser, (req, res) => {
+    const topic = 'new-booking-topic';
+    sendNotificationToTopic(topic, req.body)
     res.send()
 })
 
-app.post('/', jsonParser, (req, res) => {
+app.post('/notifications/updatedBooking', jsonParser, (req, res) => {
+    const topic = 'updated-booking-topic';
+    sendNotificationToTopic(topic, req.body)
     res.send()
 })
+
+
 app.get("/*", (req, res) => {
     res.send('Not Allowed')
 })
